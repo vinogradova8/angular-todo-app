3bda1d27bc164639aefd3077ac4696ef45c0e95b
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2025-02-06 13:25:12 +0200

    handling events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+const todos = [
+  { id: 1, title: 'HTML + CSS', completed: true },
+  { id: 2, title: 'JS', completed: false },
+  { id: 3, title: 'React', completed: false },
+  { id: 4, title: 'Vue.js', completed: true },
+];
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-todo-app';
+  todos = todos;
+  editing = false;
+
+  handleTodoToggle(event: Event, todo: Todo) {
+    todo.completed = (event.target as HTMLInputElement).checked;
+  }
+
+  get activeTodos() {
+    return this.todos.filter(todo => !todo.completed);
+  }
 }
